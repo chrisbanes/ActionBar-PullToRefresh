334c976c77e0f6544df7699e5db62d3f954ee59b
Chris Banes <chrisbanes@google.com>
2013-05-30 12:40:41 +0100

    Tidy up sample

--- sample/src/uk/co/senab/actionbarpulltorefresh/sample/AbsListViewActivity.java
+++ sample/src/uk/co/senab/actionbarpulltorefresh/sample/AbsListViewActivity.java
@@ -18,39 +18,59 @@ package uk.co.senab.actionbarpulltorefresh.sample;
 
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshHelper;
 
-public class MyActivity extends ListActivity implements PullToRefreshHelper.OnRefreshListener {
+public class AbsListViewActivity extends ListActivity implements PullToRefreshHelper.OnRefreshListener {
 
     private static String[] STRINGS = {"Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam",
-            "Abondance", "Ackawi",
-            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale",
-            "Aisy Cendre",
-            "Allgauer Emmentaler", "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam",
-            "Abondance", "Ackawi",
-            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale",
-            "Aisy Cendre",
+            "Abondance", "Ackawi", "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu",
+            "Airag", "Airedale", "Aisy Cendre", "Allgauer Emmentaler", "Abbaye de Belloc",
+            "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi", "Acorn", "Adelost",
+            "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
             "Allgauer Emmentaler"};
 
-    private PullToRefreshHelper mPullToRefreshHelper;
 
+    private PullToRefreshHelper mPullToRefreshHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ListView listView = getListView();
+        final ListView listView = getListView();
         listView.setAdapter(
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, STRINGS));
 
         mPullToRefreshHelper = new PullToRefreshHelper(this, getListView());
         mPullToRefreshHelper.setRefreshListener(this);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_hide_nav:
+                if (Build.VERSION.SDK_INT < 16) {
+                    getListView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+                } else {
+                    getListView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+                }
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -58,7 +78,7 @@ public class MyActivity extends ListActivity implements PullToRefreshHelper.OnRe
         new AsyncTask<Void, Void, Void>() {
 
             @Override
-            protected Void doInBackground(Void... voids) {
+            protected Void doInBackground(Void... params) {
                 try {
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
@@ -68,8 +88,8 @@ public class MyActivity extends ListActivity implements PullToRefreshHelper.OnRe
             }
 
             @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
                 mPullToRefreshHelper.onRefreshComplete();
             }
         }.execute();

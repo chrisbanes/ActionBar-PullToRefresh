ddef651f95cc2e1b10d0931a8d7302affd4cb5d9
Chris Banes <chris@senab.co.uk>
2013-06-16 15:27:13 +0100

    ActionBarSherlock Sample. Needs testing.

---
+++ samples/actionbarsherlock/src/uk/co/senab/actionbarpulltorefresh/samples/actionbarsherlock/ScrollViewActivity.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2013 Chris Banes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.senab.actionbarpulltorefresh.samples;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+
+import uk.co.senab.actionbarpulltorefresh.extras.actionbarsherlock.PullToRefreshAttacher;
+import uk.co.senab.actionbarpulltorefresh.samples.actionbarsherlock.R;
+
+/**
+ * This sample shows how to use ActionBar-PullToRefresh with a
+ * {@link android.widget.ScrollView ScrollView}.
+ */
+public class ScrollViewActivity extends Activity
+        implements PullToRefreshAttacher.OnRefreshListener {
+
+    private PullToRefreshAttacher mPullToRefreshAttacher;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollview);
+
+        ScrollView scrollView = (ScrollView) findViewById(R.id.ptr_scrollview);
+
+        // Create new PullToRefreshAttacher
+        mPullToRefreshAttacher = new PullToRefreshAttacher(this);
+
+        // Set Refreshable View to be the ScrollView and the refresh listener to be this.
+        mPullToRefreshAttacher.setRefreshableView(scrollView, this);
+    }
+
+    @Override
+    public void onRefreshStarted(View view) {
+        /**
+         * Simulate Refresh with 4 seconds sleep
+         */
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+
+                // Notify PullToRefreshAttacher that the refresh has finished
+                mPullToRefreshAttacher.setRefreshComplete();
+            }
+        }.execute();
+    }
+}

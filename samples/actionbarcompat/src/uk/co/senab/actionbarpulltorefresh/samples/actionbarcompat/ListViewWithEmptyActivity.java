19a31faef43175ba2fdd50ba67be3d2f15af5de8
Chris Banes <chrisbanes@google.com>
2013-09-18 15:32:13 +0100

    New ListView + Empty View Sample

---
+++ samples/actionbarcompat/src/uk/co/senab/actionbarpulltorefresh/samples/actionbarcompat/ListViewWithEmptyActivity.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2013 Chris Banes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.senab.actionbarpulltorefresh.samples.actionbarcompat;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import uk.co.senab.actionbarpulltorefresh.extras.actionbarcompat.PullToRefreshAttacher;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+
+/**
+ * This sample shows how to use ActionBar-PullToRefresh with a
+ * {@link android.widget.ListView ListView} which has an Empty View attached to it.
+ */
+public class ListViewWithEmptyActivity extends Activity
+        implements PullToRefreshAttacher.OnRefreshListener {
+
+    private static String[] ITEMS = {"Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam",
+            "Abondance", "Ackawi", "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu",
+            "Airag", "Airedale", "Aisy Cendre", "Allgauer Emmentaler", "Abbaye de Belloc",
+            "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi", "Acorn", "Adelost",
+            "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler"};
+
+    private PullToRefreshAttacher mPullToRefreshAttacher;
+
+    private ArrayAdapter<String> mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview_empty);
+
+        /**
+         * Get ListView and give it an adapter to display the sample items
+         */
+        ListView listView = (ListView) findViewById(R.id.ptr_listview);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                new ArrayList<String>());
+        listView.setEmptyView(findViewById(android.R.id.empty));
+        listView.setAdapter(mAdapter);
+
+        /**
+         * Here we create a PullToRefreshAttacher manually without an Options instance.
+         * PullToRefreshAttacher will manually create one using default values.
+         */
+        mPullToRefreshAttacher = PullToRefreshAttacher.get(this);
+
+        // Set the Refreshable View to be the ListView and the refresh listener to be this.
+        PullToRefreshLayout ptrLayout = (PullToRefreshLayout) findViewById(R.id.ptr_layout);
+        ptrLayout.setPullToRefreshAttacher(mPullToRefreshAttacher, this);
+    }
+
+    @Override
+    public void onRefreshStarted(View view) {
+        /**
+         * Simulate Refresh with 4 seconds sleep
+         */
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    Thread.sleep(Constants.SIMULATED_REFRESH_LENGTH);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+
+                mAdapter.addAll(ITEMS);
+                mAdapter.notifyDataSetChanged();
+
+                // Notify PullToRefreshAttacher that the refresh has finished
+                mPullToRefreshAttacher.setRefreshComplete();
+            }
+        }.execute();
+    }
+}

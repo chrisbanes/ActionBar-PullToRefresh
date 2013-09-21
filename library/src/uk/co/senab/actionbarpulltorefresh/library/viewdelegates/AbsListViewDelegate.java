bc512ab0f82424ab23d9dc17bc6d3d1d3795bea4
Chris Banes <chris@senab.co.uk>
2013-09-21 10:00:38 +0100

    Check whether we're starting the gesture from an AbsListView's fast scroller. Closes #86.

--- library/src/uk/co/senab/actionbarpulltorefresh/library/viewdelegates/AbsListViewDelegate.java
+++ library/src/uk/co/senab/actionbarpulltorefresh/library/viewdelegates/AbsListViewDelegate.java
@@ -16,6 +16,8 @@
 
 package uk.co.senab.actionbarpulltorefresh.library.viewdelegates;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.view.View;
 import android.widget.AbsListView;
 
@@ -30,14 +32,62 @@ public class AbsListViewDelegate
     public static final Class SUPPORTED_VIEW_CLASS = AbsListView.class;
 
     @Override
-    public boolean isReadyForPull(View view, float x, float y) {
+    public boolean isReadyForPull(View view, final float x, final float y) {
+        boolean ready = false;
+
+        // First we check whether we're scrolled to the top
         AbsListView absListView = (AbsListView) view;
         if (absListView.getCount() == 0) {
-            return true;
+            ready = true;
         } else if (absListView.getFirstVisiblePosition() == 0) {
             final View firstVisibleChild = absListView.getChildAt(0);
-            return firstVisibleChild != null && firstVisibleChild.getTop() >= 0;
+            ready = firstVisibleChild != null && firstVisibleChild.getTop() >= 0;
         }
-        return false;
+
+        // Then we have to check whether the fas scroller is enabled, and check we're not starting
+        // the gesture from the scroller
+        if (ready && absListView.isFastScrollEnabled() && isFastScrollAlwaysVisible(absListView)) {
+            switch (getVerticalScrollbarPosition(absListView)) {
+                case View.SCROLLBAR_POSITION_RIGHT:
+                    ready = x < absListView.getRight() - absListView.getVerticalScrollbarWidth();
+                    break;
+                case View.SCROLLBAR_POSITION_LEFT:
+                    ready = x > absListView.getVerticalScrollbarWidth();
+                    break;
+            }
+        }
+
+        return ready;
+    }
+
+    int getVerticalScrollbarPosition(AbsListView absListView) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                CompatV11.getVerticalScrollbarPosition(absListView) :
+                Compat.getVerticalScrollbarPosition(absListView);
+    }
+
+    boolean isFastScrollAlwaysVisible(AbsListView absListView) {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                CompatV11.isFastScrollAlwaysVisible(absListView) :
+                Compat.isFastScrollAlwaysVisible(absListView);
+    }
+
+    static class Compat {
+        static int getVerticalScrollbarPosition(AbsListView absListView) {
+            return View.SCROLLBAR_POSITION_RIGHT;
+        }
+        static boolean isFastScrollAlwaysVisible(AbsListView absListView) {
+            return false;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    static class CompatV11 {
+        static int getVerticalScrollbarPosition(AbsListView absListView) {
+            return absListView.getVerticalScrollbarPosition();
+        }
+        static boolean isFastScrollAlwaysVisible(AbsListView absListView) {
+            return absListView.isFastScrollAlwaysVisible();
+        }
     }
 }

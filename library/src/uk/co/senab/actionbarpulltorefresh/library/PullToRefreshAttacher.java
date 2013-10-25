836b9bb3d980ce28cc82cb442c22574569fc70dd
Chris Banes <chrisbanes@google.com>
2013-10-25 11:50:20 +0100

    Fix Initial X assignment

--- library/src/uk/co/senab/actionbarpulltorefresh/library/PullToRefreshAttacher.java
+++ library/src/uk/co/senab/actionbarpulltorefresh/library/PullToRefreshAttacher.java
@@ -435,9 +435,9 @@ public class PullToRefreshAttacher implements View.OnTouchListener {
             case MotionEvent.ACTION_DOWN: {
                 // If we're already refreshing, ignore
                 if (canRefresh(true, params.getOnRefreshListener())
-                        && params.mViewDelegate.isReadyForPull(view, event.getX(), event.getY())) {
+                        && params.getViewDelegate().isReadyForPull(view, event.getX(), event.getY())) {
                     mInitialMotionY = (int) event.getY();
-                    mInitialMotionX = (int) event.getY();
+                    mInitialMotionX = (int) event.getX();
                 }
                 break;
             }
